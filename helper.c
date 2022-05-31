1477d5fb9aad802fe55238ffbb71feb8c9a7be6f
NoGoat <74340622+TheNoGoat@users.noreply.github.com>
2022-05-31 11:06:56 +0530

    Update helper.c

--- helper.c
+++ helper.c
@@ -1,9 +1,13 @@
 //This is just a random script that I'm writing to learn C.
 //Don't expect too much functionality out of it.
+
+//Edit on May 31, 2022 11:04 PM. The code is horribly incomplete. Don't use this, it won't work.
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
+#include <string.h>
 
+int set[10];
 char getch(void)  //Created this since Linux doesn't have getch() functionality.
 {                 //Code : https://stackoverflow.com/questions/7469139/what-is-the-equivalent-to-getch-getche-in-linux?noredirect=1&lq=1
     char buf = 0;
@@ -25,33 +29,157 @@ char getch(void)  //Created this since Linux doesn't have getch() functionality.
         perror("tcsetattr ~ICANON");
     return buf;
 }
-void main()
+void update()
 {
-    int c,k,s;
+    system("clear");
+    printf("\n\n\t\tPerforming Full System Upgrade.\n\n\t\tUpdating Repos.\n\n");
+    if (set[0] == 1)
+    {
+        printf("\033[0;31m");
+        printf("\n\n\tCommand : "); 
+        printf("\033[0;32m");
+        printf("sudo apt update\n\n");
+        printf("\033[0m"); 
+    }
+    fflush(stdin);
+    system("sudo apt update");
+    fflush(stdin);
+    printf("\n\n\t\tStarting the actual Upgrade Process.\n\n");
+    fflush(stdin);
+    if (set[0] == 1)
+    {
+        fflush(0);
+        printf("\033[0;31m\n\n\tCommand : \033[0;32msudo apt upgrade \033[0m \n\n");
+    }
+    system("sudo apt upgrade");
+    printf("\n\n\t\tPress any key to continue.");
+    fflush(stdin);
+    getch();
+}
+void settings()
+{
+    FILE *setr,*setw;
+    char path[] = "preferences.txt";
+    int c;
+    int ch;
+    char read[1000], sett[1000];
+    int k,i,pos=0,j;
     while(1)
     {
         system("clear");
-        fflush(0);
+        printf("\n\n\t\tSettings");
+        printf("\n\n\t1. Show Commands");
+        if(set[0] == 0)
+        {
+            printf("\t[ ]");
+        }
+        else if(set[0] == 1)
+        {
+            printf("\t[*]");
+        }
+        printf("\n\n\t2. Exit");
+        printf("\n\nEnter a Choice to Change :");
+        scanf("%d",&c);
+        fflush(stdin);
+        switch (c)
+        {
+            case 1: system("clear");
+                    printf("\n\n\t\tSettings");
+                    printf("\n\n\t1. Show Commands");
+                    if(set[0] == 0)
+                    {
+                        printf("\t[ ]");
+                    }
+                    else if(set[0] == 1)
+                    {
+                        printf("\t[*]");
+                    }
+                    printf("\n\n\t2. Exit");
+                    printf("\n\nDo you want to change this setting [Y\\n] :");
+                    fflush(stdin);
+                    scanf("%d",&ch);
+                    
+                    if(ch == 1)
+                    {
+                        if(set[0]==1)
+                        {
+                            set[0]=0;
+                        }
+                        else if(set[0]==0)
+                        {
+                            set[0]=1;
+                        }
+                    }
+                    break;
+            case 2: return;
+        }
+    }
+    return;
+}
+void main()
+{
+    int c,k,i,pos=0,j;
+    char path[] = "preferences.txt";
+    char read[1000], sett[1000];
+    while(1)
+    {
+        FILE *checkw,*checkr;
+        if(fopen(path,"r") == NULL)
+        {
+            //system("mkdir ~/.config/helper");
+            fflush(stdin);
+            printf("\nTest1\n");
+            checkw = fopen(path,"w");
+            printf("\nTest2\n");
+            fflush(stdin);
+            printf("\nTest3\n");
+            fprintf(checkw,"ShowCommands:0;");
+            printf("\nTest4\n");
+            fflush(stdin);
+            fclose(checkw);
+            fflush(stdin);
+        }
+        checkr = fopen(path,"r");
+        fflush(stdin);
+        fgets(read, 1000, checkr);
+        fflush(stdin);
+        for(i = 0 ; i < strlen(read) ; i++)
+        {
+            if(read[i] == ";")
+            {
+                for (j=pos ; j < i-1 ; j++)
+                {
+                    sett[j] == read[j];    
+                }
+                pos = i;
+                if (sett[j] == "ShowCommands");
+                {
+                    if(read[i-1] == "0")
+                    {
+                        set[0] = 0;
+                    }
+                    else if(read[i-1] == "1")
+                    {
+                        set[0] = 0;
+                    }
+                }
+            }
+        }
+        system("clear");
+        fflush(stdin);
         printf("\n\n\n\t\tEnter a Choice :");
         printf("\n\n\t1. Perform a full system upgrade.");
-        printf("\n\n\t2. Exit this script.");
+        printf("\n\n\t2. Change the settings file.");
+        printf("\n\n\t3. Exit this script.");
         printf("\n\nEnter a Choice :");
         scanf("%d",&c);
         switch (c)
         {
-            case 1: system("clear");
-                    printf("\n\n\t\tPerforming Full System Upgrade.\n\n\t\tUpdating Repos.\n\n");
-                    fflush(0);
-                    system("sudo apt update");
-                    fflush(0);
-                    printf("\n\n\t\tStarting the actual Upgrade Process.\n\n");
-                    fflush(0);
-                    system("sudo apt upgrade");
-                    printf("\n\n\t\tPress any key to continue.");
-                    fflush(0);
-                    getch();
+            case 1: update();
                     break;
-            case 2: system("clear");
+            case 2: settings();
+                    break;
+            case 3: system("clear");
                     return;
         }
     }
