0483c4276de2e259bb87492fc0b64768eabc10ff
NoGoat <74340622+TheNoGoat@users.noreply.github.com>
2022-06-02 14:43:05 +0530

    Update helper-installer.sh
    Modified it so that it will automatically remove an old version and install the new version.

--- helper-installer.sh
+++ helper-installer.sh
@@ -1,5 +1,17 @@
+echo "Helper will be installed to $HOME/.helper."
+echo "A symlink will be created in /bin."
+echo "Options will arrive at a later date."
+if [ -d ~/.helper ]; then
+    if [ -e ~/.helper/helper ]; then
+        rm ~/.helper/helper
+    fi
+    rmdir ~/.helper
+fi
 mkdir ~/.helper
 cp helper ~/.helper
 cd ~/.helper
+if [ -e /bin/helper ]; then
+    sudo rm /bin/helper
+fi
 cwd=$(pwd)
 sudo ln -s $cwd"/helper" /bin/
