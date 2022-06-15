f7b99e3dc7f4e54ef74f8ec3b346e2119145c5a7
NoGoat <74340622+TheNoGoat@users.noreply.github.com>
2022-06-15 11:27:55 +0530

    Fixed it to work with minthelper instead of helper

--- helper-installer.sh
+++ helper-installer.sh
@@ -7,11 +7,20 @@ if [ -d ~/.helper ]; then
     fi
     rmdir ~/.helper
 fi
+if [ -d ~/.helper ]; then
+    if [ -e ~/.helper/minthelper ]; then
+        rm ~/.helper/helper
+    fi
+    rmdir ~/.helper
+fi
 mkdir ~/.helper
-cp helper ~/.helper
+cp minthelper ~/.helper
 cd ~/.helper
 if [ -e /bin/helper ]; then
     sudo rm /bin/helper
 fi
+if [ -e /bin/minthelper ]; then
+    sudo rm /bin/minthelper
+fi
 cwd=$(pwd)
-sudo ln -s $cwd"/helper" /bin/
+sudo ln -s $cwd"/minthelper" /bin/
